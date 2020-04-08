ce94e97cd1c399f4a202be33b29d5380f33b4656
Stephan <stephan.savchenko@gmail.com>
2020-04-08 15:53:44 +0300

    Lab2 tree initialization, printing and memory cleaning  added

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -11,7 +11,7 @@
 struct Node {
 
     std::string name;
-    std::vector<Node> children;
+    std::vector<Node*> children;
 
     /*
 
@@ -40,9 +40,20 @@ struct Tree {
     int size = 0;
 };
 
+std::string getWeight( Node* node ) {
+    std::string res = "";
+    for (int num : node->weight) {
+        /* code */
+    }
+    return res;
+}
+
+
 bool enter_node( Node *node ) {
 
     printf("\nNode name: ");
+    counter++;
+
     std::cin >> node->name;
     if (node->name == "_") { return false; }
 
@@ -63,7 +74,6 @@ bool enter_node( Node *node ) {
     } else {
         node->weight.push_back(num);
     }
-    printf("\033[0;31m--------------------- Test point 0 ---------------------\033[0m\n");
     std::cout << "New node name: " << node->name << " New node weight size: " << node->weight.size() << '\n';
 
     return true;
@@ -71,29 +81,42 @@ bool enter_node( Node *node ) {
 
 bool init_node( Node *node ) {
 
-    Node new_node;
     bool res = false;
+
+    printf("\033[0;34m");
+    std::cout << "\n\n--------- " << node->name << " NODES CHILDREN INITIALIZATION ---------" << '\n';
+    printf("\033[0m");
 
     for (unsigned short i = 0; i < 2 ; i++) {
 
-        if (!enter_node( &new_node )) {
-            printf("\033[0;31m--------------------- Test point 1 ---------------------\033[0m\n");
+        Node *new_node = new Node;
+
+        if (!enter_node( new_node )) {
+            free(new_node);
+            printf("\033[0;32mNode creation canceled. Memory cleaned.\033[0m\n");
             continue;
          }
 
-        printf("\033[0;31m--------------------- Test point 2 ---------------------\033[0m\n");
-        std::cout << "New node name: " << new_node.name << " New node weight size: " << new_node.weight.size() << '\n';
+         // Checking if first node was lighter
+         if ( i == 1) {
+             if (node->children[0]->weight[0] > new_node->weight[0]) {
+
+                 std::cout << "\033[0;31m\nSecond child shuld be heavier than first\033[0m\n"
+                           << "Fist w8: " << node->children[0]->weight[0] << " name: " << node->children[0]->name
+                           << "\nSecond w8: " << new_node->weight[0] << " name: " << new_node->name << '\n';
+                 i--;
+                 free(new_node);
+             }
+         }
 
         node->children.push_back(new_node);
         printf("\033[0;32mNode created\033[0m\n");
         res = true;
 
-        new_node = Node();
-
     }
 
     for (unsigned short i = 0; i < node->children.size() ; i++) {
-        init_node( &(node->children[i]) );
+        init_node( node->children[i] );
     }
 
     return res;
@@ -102,58 +125,121 @@ bool init_node( Node *node ) {
 
 bool init_tree( Tree *tree ) {
 
-    Node root;
-    tree->root = &root;
+    Node *root = new Node;
+    tree->root = root;
 
     printf("\nTree name: ");
     std::cin >> tree->name;
     if (tree->name == "_") { return false; }
 
-    if (!enter_node( &root )) { return false; }
-    
-    if (!init_node( &root )) { return false; }
+    printf("\nStart from root:\n");
+    if (!enter_node( root )) { return false; }
+
+    printf("\033[0;34m\n\n--------- ROOT NODES CHILDREN INITIALIZATION ---------\033[0m");
+    if (!init_node( root )) { return false; }
 
      return true;
 
 }
 
-bool node_print( Node* node ) {
+bool node_print( Node* node, int level ) {
 
-    std::cout << "Name: " << node->name << '\n';
-    std::cout << "Weight size: " << node->weight.size() << '\n';
+    if (!node) {
+        level--;
+        return false;
+     }
 
-    for (Node node : node->children) {
-        node_print( &node );
+     std::string indentation = "";
+     for (int i = 0; i < level; i++) {
+         indentation += "\t";
+     }
+    std::cout << '\n' << indentation << "---------- Node: " << node->name << node->weight.size() << " ----------\n"
+    << indentation << "Children nodes:" << '\n';
+
+    level++;
+    for (Node *node : node->children) {
+        node_print( node, level );
     }
 
+    return true;
 }
+
+
+/*
+
+T:
+            A
+      B            C
+    D    E      F    G
+H    I J   K   L  M N  O
+
+
+T2:
+        1
+  2          3
+4   5     6    7
+
+
+*/
 
 bool tree_print( Tree *tree ) {
 
+    printf("\033[0;34m\n\n--------- TREE PRINT ---------\033[0m\n");
     std::cout << "Name: " << tree->name << '\n';
     std::cout << "Size: " << tree->size << '\n';
 
-    return node_print( tree->root );
+    return node_print( tree->root, 1 );
 
 }
 
+bool delete_node( Node* node ) {
 
+    if (!node) { return false; }
+
+    for (Node *node : node->children) {
+        delete_node( node );
+    }
+
+    // std::cout << "---------- Node: " << node->name << node->weight.size() << " Deleted ----------\n";
+    free(node);
+
+    return true;
+
+}
+
+bool clear_memory( Tree* tree ) {
+
+    if (!tree) { return false; }
+
+    if (!delete_node(tree->root)) { return false; }
+
+    // std::cout << "---------- Node: " << tree->name << tree->size << " Deleted ----------\n";
+    free(tree);
+
+    return true;
+
+}
 
 
 int main(int argc, char **argv) {
 
-    Tree my_tree;
+    Tree *my_tree = new Tree;
 
     printf("\033[0;34m--------- TREE INITIALIZATION ---------\033[0m\n");
 
-    if (!init_tree( &my_tree )) {
+    if (!init_tree( my_tree )) {
         printf("\033[0;31mTree init error\033[0m\n");
     }
 
-    // if (!tree_print( &my_tree )) {
-    //     printf("\033[0;31mTree print error\033[0m\n");
-    // }
+    if (!tree_print( my_tree )) {
+        printf("\033[0;31mTree print error\033[0m\n");
+    }
 
-    // delete
+    if (!clear_memory( my_tree )) {
+        printf("\033[0;31mTree was empty, or was not exist.\033[0m\n");
+    } else {
+        printf("\033[0;32mMemory cleaned\033[0m\n");
+    }
+
     return 0;
 }
