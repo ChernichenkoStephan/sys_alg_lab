b71254f66177f0f60ba96047d098515d062dd9b4
Stephan <stephan.savchenko@gmail.com>
2020-04-12 19:11:15 +0300

    lab2 v2

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -11,36 +11,60 @@
 
 
 
-vector<size_t> prefix_function (string s) {
+std::vector<size_t> prefix_function (std::string s) {
 
-	int n = (int) s.length();
+	int n = (int)s.length();
 
-	vector<size_t> P(n);
+	std::vector<size_t> p_array(n);
 
 	for (size_t i = 1; i < n; ++i) {
 
-		size_t j = pi[i-1];
+		size_t j = p_array[i-1];
 
 		while ( j > 0 && (s[i] != s[j]) ) {
-            j = pi[j-1];
+            j = p_array[j-1];
         }
 
 		if (s[i] == s[j]) { ++j; }
 
-		pi[i] = j;
+		p_array[i] = j;
 
 	}
-	return pi;
+	return p_array;
 }
 
 
+bool kmp_search() {
+	std::string s, key;
 
+	std::cout << "Enter investigation string" << '\n';
+    std::cin >> s;
+	std::cout << "Enter string to find" << '\n';
+	std::cin >> key;
+
+    std::vector<size_t> p_array = prefix_function(key + '#' + s);
+
+    int key_len = key.length();
+
+    for (int i = 0; i < s.length(); i++) {
+        if (p_array[key_len + 1 + i] == key_len) {
+
+			size_t start_index = i - key_len + 1;
+			size_t end_index = i;
+
+			std::cout << "Key found at index: "<< start_index << std::endl;
+        }
+    }
+	return true;
+}
 
 
 
 int main(int argc, char **argv) {
 
     system("clear");
+
+	kmp_search();
 
     return 0;
 }
